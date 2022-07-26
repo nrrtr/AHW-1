681ce350d14c57563fb808bf775863d21e437d88
nrr <nrr@warsong.me>
2022-07-26 17:12:25 +0700

    added JUnitPlatform implementation and change tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,11 +1,9 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
-    @Test
+    @org.junit.Test
     public void shouldReturn1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(0);
@@ -13,7 +11,15 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
+    public void shouldReturn1000api() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(0);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
     public void shouldReturn0() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(1000);
@@ -21,7 +27,15 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
+    public void shouldReturn0api() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
     public void shouldReturn1() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(999);
@@ -29,8 +43,24 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
+    public void shouldReturn1api() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(999);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
     public void shouldReturn999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(1);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn999api() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(1);
         int expected = 999;
